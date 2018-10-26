24364f39f69a7d6aac68820781f5e7eb55dac832
SongChic <songchic2@gmail.com>
2018-10-26 18:43:47 +0900

    위치 권한 허용 코드 추가

--- app/src/main/java/com/ahqlab/xvic/activity/Splash.java
+++ app/src/main/java/com/ahqlab/xvic/activity/Splash.java
@@ -1,9 +1,15 @@
 package com.ahqlab.xvic.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import com.ahqlab.xvic.MainActivity;
 import com.ahqlab.xvic.R;
@@ -16,17 +22,55 @@ public class Splash extends BaseActivity<Splash> {
         this.noActionBar();
         setContentView(R.layout.activity_splash);
 
-        /* 5초 뒤에 메인으로 이동
-         * 실제 코드 작성 시 데이터를 받아와서 이동 */
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                move();
-            }
-        }, 5000);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2000);
+            return;
+        } else {
+            /* 5초 뒤에 메인으로 이동
+             * 실제 코드 작성 시 데이터를 받아와서 이동 */
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    move();
+                }
+            }, 5000);
+        }
+
     }
     private void move() {
         startActivity(new Intent(this, MainActivity.class));
         finish();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int i = 0; i < permissions.length; i++) {
+            String permission = permissions[i];
+            int grantResult = grantResults[i];
+
+            if (permission.equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                    /* 5초 뒤에 메인으로 이동
+                     * 실제 코드 작성 시 데이터를 받아와서 이동 */
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            move();
+                        }
+                    }, 5000);
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2000);
+                }
+            }
+        }
+    }
+
+
 }
